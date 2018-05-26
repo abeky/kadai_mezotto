f40f408aee1bd4dc6547e657a1445f97fbb839cf
abeky <kazuma.abe01@gmail.com>
2018-05-26 18:41:38 +0900

    Initial Commit

---
+++ kadai_mezotto/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  kadai_mezotto
+//
+//  Created by 阿部一真 on 2018/05/26.
+//  Copyright © 2018年 阿部一真. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
