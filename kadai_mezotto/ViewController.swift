4e2c09a447ea0a1921e809ea32d35be812e53c23
abeky <kazuma.abe01@gmail.com>
2018-06-06 16:07:29 +0900

    aa

--- kadai_mezotto/ViewController.swift
+++ kadai_mezotto/ViewController.swift
@@ -9,17 +9,31 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var text1: UITextField!
+    @IBOutlet weak var text2: UITextField!
+    @IBOutlet weak var label: UILabel!
+    @IBOutlet weak var keisan: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        
     }
-
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
-
+    
+    @IBAction func keisanbutton(){
+        let x1 = Int(text1.text!)
+        let x2 = Int(text2.text!)
+        
+        let y = x1! + x2!
+        
+        label.text = String(y)
+            
+    }
 }
-
